7a4634bcd080456d3a5d93f33829f275e7c708cc
Ömer Akıncı <mer Aknc@DESKTOP-BMO5C0U>
2021-03-12 07:08:22 +0300

    Add project files.

---
+++ MyDictionary/Mylist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Mylist<K, V>
+    {
+        K[] _arrayKey;
+        V[] _arrayValue;
+        public Mylist()
+        {
+            _arrayKey = new K[0];
+            _arrayValue = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempKey = _arrayKey;
+            _arrayKey = new K[tempKey.Length + 1];
+
+            V[] tempValue = _arrayValue;
+            _arrayValue = new V[tempValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _arrayKey[i] = tempKey[i];
+                _arrayValue[i] = tempValue[i];
+            }
+
+            _arrayKey[_arrayKey.Length - 1] = key;
+            _arrayValue[_arrayValue.Length - 1] = value;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mylist<int, string> sehirler = new Mylist<int, string>();
+            sehirler.Add(01, "Adana");
+            sehirler.Add(02, "Adıyaman");
+
+        }
+    }
+}
